6bcaec34f87af7db3738d6574b57e9aadc1d7ec1
sana srikar <srikarsana1995@gmail.com>
2018-09-14 14:02:36 +0530

    Update chardriver.c

--- File_01/chardriver.c
+++ File_01/chardriver.c
@@ -3,6 +3,7 @@
 #include<linux/kernel.h>
 #include<linux/init.h>
 #include<linux/types.h>
+#include<linux/cdev.h>
 #include<linux/fs.h>
 #include<linux/device.h>
 #include<linux/uaccess.h>
@@ -19,9 +20,9 @@ static ssize_t drv_write(struct file *file, const char *buf ,size_t len, loff_t 
 /*a variable to store the device number */
 dev_t dev=0; //device number = MKDEV(major,minor)
 int baseminor=0,count=1;
-
-static struct class *dev_class;
-
+int major=0;
+static struct class *dev_class =NULL;
+static struct device *sr_device= NULL;
 
 struct file_operations fops=
 {
@@ -64,21 +65,40 @@ static ssize_t drv_write(struct file *file, const char *buf ,size_t len, loff_t 
 static int __init drv_init(void)
 {
         int retval=0;
-        /* registering the device using __register_chrdev API */
-        retval= __register_chrdev(&dev,baseminor,count,"Srikar_Devicedrv",&fops);
-        if(retval)
+        /*
+         * registering the device using __register_chrdev API
+         * We will be using dynamic allocation of major number by sending the major =0
+         * If this is succesfull then it returns the major number or else we will get 
+         * negative number.
+         * So we shall write a longcode to know where it failed.
+         * if it returns '0' then it means we did not got allocation of major number.
+         */
+        
+        retval = __register_chrdev(major,baseminor,count,"sr_Devicedrv",&fops);
+        
+        if(retval<0)
         {
             /* if failed just return the error number */
-            printk(KERN_ALERT"device registration failed\n");
+            printk(KERN_EMERG"sr_Devicedriver device registration failed %d \n",retval);
+            goto reg_failed;
+        }
+        else if(retval ==0)
+        {
+            /* if succesfully registered print message in the log*/
+            printk(KERN_EMERG"sr_Devicedriver sr_Devicedrv registered succesfully retval =%d\n",retval);
+            retval =-1;
             goto reg_failed;
         }
         else
         {
-            /* if succesfully registered print message in the log*/
-            printk(KERN_ALERT"Srikar_Devicedrv registered succesfully retval =%d\n",retval);
+            /* the execution has come here then it means that we have reigstered our device*/
+            major =retval;
+            dev =MKDEV(major, 0);
+            printk(KERN_ALERT"sr_Devicedriver registered succesfully with a major %d",major);
+            retval=0; /*Intialize it again*/
         }
         
-        dev_class= class_create(THIS_MODULE,"Srikar_class");
+        dev_class= class_create(THIS_MODULE,"sr_class");
         /*how to know whether class_Create has failed
          * 1.use IS_ERR() to know whether the return value contains error or not
          * 2.use PTR_ERR() macro to know the error number
@@ -86,54 +106,53 @@ static int __init drv_init(void)
         if(IS_ERR(dev_class))
         {
            retval= PTR_ERR(dev_class);
-           printk(KERN_ALERT"Srikar_class failed to create retval =%d\n",retval);
+           printk(KERN_EMERG"sr_Devicedriver sr_class failed to create retval =%d\n",retval);
            goto cls_failed;
         }
         else
         {
-            printk(KERN_ALERT"Srikar_class creation is succesfully \n");
+            printk(KERN_EMERG"sr_Devicedriver sr_class creation is succesfully \n");
         }
         /* Lets create device using device_create()
          * for now one device is fine.
          */
           
-        retval=device_create(/*class*/dev_class, /*parent*/NULL,/*private data*/NULL,"%s","Srikar_device");
-        if(retval)
+        sr_device =device_create(/*class*/dev_class, /*parent*/NULL, /*device number*/ dev, /*private data*/ NULL, "srdevice" );
+        if(IS_ERR(sr_device))
         {
-		goto devcrt_failed;
-           printk(KERN_ALERT"Device Srikar_device creation failed retval =%d",retval);
-	
-            
+           retval= PTR_ERR(sr_device);
+           printk(KERN_EMERG"sr_Devicedriver sr_class failed to create retval =%d\n",retval);
+           goto devcrt_failed;
         }
         else
         {
-            
-           printk(KERN_ALERT"Device Srikar_device is succesfully created retval =%d",retval);
+            printk(KERN_EMERG"sr_Devicedriver sr_class creation is succesfully \n");
         }
+        
 devcrt_failed:
     class_destroy(dev_class);
 cls_failed:
-    unregister_chrdev(MAJOR(dev),"Srikar_Devicedrv");
+    unregister_chrdev(MAJOR(dev),"sr_Devicedrv");
 reg_failed:
-	printk(KERN_ALERT"%d is the retval before init exits ",retval);
+	printk(KERN_EMERG"sr_Devicedriver %d is the retval before init exits ",retval);
     return retval;
 }
 
 static void __exit drv_exit(void)
 {
-    printk(KERN_ALERT"Srikar_Devicedrv into exit function");
+    printk(KERN_EMERG"sr_Devicedriver sr_Devicedrv into exit function");
     /*first destroy the device*/
     device_destroy(dev_class, dev);
     /* then destroy the class */
     class_destroy(dev_class);
     /*last ,unregister the device*/
-    unregister_chrdev(MAJOR(dev),"Srikar_Devicedrv");
+    unregister_chrdev_region(dev,count);
 }
 
 module_init(drv_init);
 module_exit(drv_exit);
 
 /* module info */
-MODULE_AUTHOR("SRIKAR SANA");
+MODULE_AUTHOR("sr SANA");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Srikar char device driver");
+MODULE_DESCRIPTION("sr char device driver");
